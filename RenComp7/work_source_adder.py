e5e31c175274bac85d4730b15f6e378d5673f547
juyaolongpaul <yaolong.ju@mail.mcgill.ca>
2020-02-04 18:16:29 -0500

    Finding the right path when not found

--- RenComp7/work_source_adder.py
+++ RenComp7/work_source_adder.py
@@ -5,7 +5,7 @@ from datetime import date
 import csv
 
 proj_path = "../../"
-#print(os.getcwd())
+original_cwd = os.getcwd()
 # This is so mpythoy local_settings.py gets loaded.
 os.chdir(proj_path)
 
@@ -35,8 +35,21 @@ from database.models.genre_as_in_style import GenreAsInStyle
 from database.models.contribution_musical_work import ContributionMusicalWork
 from database.models.genre_as_in_type import GenreAsInType
 from database.models.source_instantiation import SourceInstantiation
-from sample_data_for_SIMSSA_DB.common_function import parseSource
-from sample_data_for_SIMSSA_DB.common_function import createContribution
+from sample_data_for_SIMSSA_DB.Florence_164.work_source_adder import parseSource
+
+
+def createContribution(p, work):
+    """
+
+    :return:
+    """
+    contribute = ContributionMusicalWork.objects.get_or_create(
+        person=p,
+        certainty_of_attribution=True,  # We assume these pieces are all secure
+        role='COMPOSER',
+        contributed_to_work=work
+    )[0]
+    return contribute
 
 
 def addPiece(given_name_input, surname_input, birth_input, death_input, viaf_url_input, folder_name, counter, header):
@@ -74,7 +87,7 @@ def addPiece(given_name_input, surname_input, birth_input, death_input, viaf_url
 
     counter_same_file = 1
     for file_name_all in os.listdir(
-            os.path.join(os.getcwd(), 'sample_data_for_SIMSSA_DB', 'RenComp7', folder_name)):  # iterate each file within the folder
+            os.path.join(os.getcwd(), folder_name)):  # iterate each file within the folder
         if '.DS_Store' in file_name_all: continue
         counter += 1
         file_name, file_extension = os.path.splitext(file_name_all)
@@ -118,7 +131,7 @@ def addPiece(given_name_input, surname_input, birth_input, death_input, viaf_url
         # remove the unnecessary numbering for sections
         # Find the metadata in the CSV file
         found_entry = False
-        with open(os.path.join(os.getcwd(), 'sample_data_for_SIMSSA_DB', 'RenComp7', 'RenComp7_metadata_IL_no_duplicate.csv')) as csvfile:
+        with open(os.path.join(os.getcwd(), 'RenComp7_metadata_IL_no_duplicate.csv')) as csvfile:
             readCSV = csv.reader(csvfile, delimiter=',')
             rows = []
             for row in readCSV:
@@ -186,7 +199,7 @@ def addPiece(given_name_input, surname_input, birth_input, death_input, viaf_url
         else:
             source_instantiation = SourceInstantiation.objects.get_or_create(source=source, portion=str(counter),
                                                                              work=work)[0]
-        file_path = os.path.join(os.getcwd(), 'sample_data_for_SIMSSA_DB', 'RenComp7', folder_name, file_name_all)
+        file_path = os.path.join(os.getcwd(), folder_name, file_name_all)
         if file_extension == '.xml':
             file_local = open(file_path)
         else:
@@ -213,9 +226,13 @@ if __name__ == "__main__":
     mediatype = 'symbolic_music/'
     mediapath = getattr(settings, "MEDIA_ROOT", None)
     mediapath = mediapath + mediatype
-    #os.chdir(os.path.dirname(os.path.abspath(__file__)))
     counter = 0
-    all_folders = os.listdir(os.path.join(os.getcwd(), 'sample_data_for_SIMSSA_DB', 'RenComp7'))
+    print('os.getcwd', os.getcwd())
+    print('the current file directory', os.path.abspath(__file__))
+    if os.getcwd() == '/':
+        os.chdir(os.path.join('code', 'simssadb', 'sample_data_for_SIMSSA_DB', 'RenComp7'))
+    all_folders = os.listdir(os.getcwd())
+    print('all folders', all_folders)
     # Create CSV file to export the metadata to check
     header = [
         ['File Name', 'Composer Given Name', 'Composer Surname', 'Musical Work Name', 'Section Name',
@@ -226,6 +243,7 @@ if __name__ == "__main__":
                 in folder_name:
             continue
         else:
+            print('current folder', folder_name)
             if folder_name == 'Giovanni_Pierluigi_da_Palestrina':  # this one has different syntax
                 given_name_input = 'Giovanni Pierluigi da'
                 surname_input = 'Palestrina'
@@ -279,3 +297,4 @@ if __name__ == "__main__":
     #                        'RenComp7_metadata.csv'), 'w') as csvFile:
     #     writer = csv.writer(csvFile)
     #     writer.writerows(header)
+    os.chdir(original_cwd)
